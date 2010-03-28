db7236159d1eee0885577721a52597500b75b0f5
Yaniv Aknin <yaniv@aknin.name>
2010-03-28 19:42:55 +1100

    separate tests to basic and cli-based

--- basic.py
+++ basic.py
@@ -5,37 +5,15 @@ import argparse
 import httplib
 
 from labour import servers
-from labour.servers import servers as servers_map
 from labour import client
 from labour import behaviours
 from labour import report
-from labour import log as log
-from labour.errors import main_error_handler
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--server', choices=servers_map, default='WSGIRef')
-    parser.add_argument('-i', '--iterations', type=int, default=1000)
-    options = parser.parse_args(argv)
-    options.server = servers_map[options.server]
-    return options
+driver = client.Client()
+driver.add_behaviour(behaviours.PlainResponse(), weight=99)
+driver.add_behaviour(behaviours.Sleeping(sleep_duration=0.5), weight=1)
 
-@main_error_handler
-def main(argv):
-    log.setup_logging()
-    options = parse_arguments(argv[1:])
+with servers.WSGIRef as server:
+    driver.execute(iterations=options.iterations)
 
-    with options.server() as server:
-        driver = client.Client()
-        driver.add_behaviour(behaviours.PlainResponse(), weight=96)
-        driver.add_behaviour(behaviours.PlainResponse(status=httplib.INTERNAL_SERVER_ERROR), weight=1)
-        driver.add_behaviour(behaviours.PlainResponse(status=httplib.FORBIDDEN), weight=1)
-        driver.add_behaviour(behaviours.PlainResponse(status=httplib.NOT_FOUND), weight=1)
-        driver.add_behaviour(behaviours.Sleeping(sleep_duration=0.5), weight=1)
-
-        driver.execute(iterations=options.iterations)
-
-    report.trivial_report(driver)
-
-if __name__ == '__main__':
-    main(sys.argv)
+report.trivial_report(driver)
